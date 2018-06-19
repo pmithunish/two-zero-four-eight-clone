9f4f4a5268d018af17f97dd357679759de0f23e9
Mithunish Prabhakaran <mithun@animapp.in>
2018-06-20 03:40:26 +0530

    25 percent work is completed but game is buggy apparantly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule
+} from '@angular/material';
+
+const Components = [AppComponent, HomeComponent, PageNotFoundComponent];
+
+const MaterialModules = [MatButtonModule, MatCardModule, MatIconModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: Components,
+  imports: [BrowserModule, AppRoutingModule, ...MaterialModules],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
